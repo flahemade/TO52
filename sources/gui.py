d2aecd10fdf4190d4ca931961a983400b123b48b
unknown <felix.lahemade@gmail.com>
2015-10-06 14:50:37 +0200

    Updated GUI.

--- sources/gui.py
+++ sources/gui.py
@@ -1,10 +1,32 @@
 __author__ = 'Iki'
 
+from tkFileDialog import askopenfilename
 from Tkinter import *
 
-fenetre = Tk()
+def click(event):
+            filepath = askopenfilename(title="Import a pdf",filetypes=[('pdf files','.pdf'),('all files','.*')])
+            print filepath
+            message2=Label(window,text=filepath)
+            message2.pack()
+            button_import.pack_forget()
+            button_analyse=Button(window,text="Analyse")
+            button_analyse.pack(side="bottom")
 
-label = Label(fenetre, text="TO52")
-label.pack()
+window = Tk()
 
-fenetre.mainloop()
+button_import=Button(window,text="Import")
+button_import.bind("<Button-1>",click)
+button_import.pack(side="left")
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
