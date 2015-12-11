57d38317d8c05b240e1e853c189d2d0f6d882612
unknown <felix.lahemade@gmail.com>
2015-12-11 21:05:49 +0100

    Changed upper to lower. Added UTF-8 encoding

--- sources/TO52/Analyse.py
+++ sources/TO52/Analyse.py
@@ -39,7 +39,7 @@ class Analyse:
                 for sword in sub_word:
                     if len(sword)>0:
                         if ((ord(sword[0])>=65) and (ord(sword[0])<=90)):
-                           
+                            sword = sword.lower()
                             if i == 78 or i == 79:
                                 print previous_word
                                 print sword
