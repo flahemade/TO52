64a593c403a321da1180002d72e5e8320962162b
Julien Robert <bob.julien020@gmail.com>
2015-12-20 14:10:17 +0100

    fin heuristique de base

--- sources/TO52/Analyse.py
+++ sources/TO52/Analyse.py
@@ -28,54 +28,52 @@ class Analyse:
         for page in book.pages:
             i += 1
             text = page.text
-
-            if i == 78:
-                print text
             words = text.split()
             words = words[0:-1]
-            previous_word = " "
+            previous_word = "."
             for word in words:
                 word = word.replace("'","-")
                 sub_word = word.split('-')
                 for sword in sub_word:
-                    if len(sword)>0:
+                    if len(sword)>1:
                         if ((ord(sword[0])>=65) and (ord(sword[0])<=90)):
-                            sword.lower()
-                            if i == 78 or i == 79:
-                                print previous_word
-                                print sword
-                            if 33 <= ord(sword[-1]) <= 64:
-                                    sword = sword[0:-1]
-
-                            if not Analyse.not_perso.__contains__(sword):
-                                if sword not in Analyse.perso.keys():
-                                    if sword in perso_tmp.keys():
-                                        perso_tmp.get(sword).append(i)
-                                        if (not previous_word[-1] == '.') and (not previous_word[-1] == '?') and (not previous_word[-1] == '!') and (not previous_word[-1] == '-') and (not previous_word[-1] == '"'):
-                                            Analyse.perso[sword] = perso_tmp.get(sword)
-                                            del perso_tmp[sword]
-                                    else:
-                                        if (not previous_word[-1] == '.') and (not previous_word[-1] == '?') and (not previous_word[-1] == '!') and (not previous_word[-1] == '-') and (not previous_word[-1] == '"'):
-                                            Analyse.perso[sword] = [i]
+                            sword = sword.replace("É","é")
+                            sword = sword.replace("È","è")
+                            sword = sword.replace("À","à")
+                            sword = sword.lower()
+                            while(not ((65<=ord(sword[-1])<=90) or (97<=ord(sword[-1])<=122) or (sword[-2:]=='é'))):
+                                sword = sword[0:-1]
+                            if len(sword)>1:
+                                if not Analyse.not_perso.__contains__(sword):
+                                    if sword not in Analyse.perso.keys():
+                                        if sword in perso_tmp.keys():
+                                            perso_tmp.get(sword).append(i)
+                                            if (not previous_word[-1] == '.') and (not previous_word[-1] == '?') and (not previous_word[-1] == '!') and (not previous_word[-3:] == '—') and (not previous_word[-3:] == '«') and (not previous_word[-3:] == '…') and (not previous_word[-1] == ':'):
+                                                Analyse.perso[sword] = perso_tmp.get(sword)
+                                                del perso_tmp[sword]
                                         else:
-                                            perso_tmp[sword] = [i]
-                                else:
-                                    Analyse.perso.get(sword).append(i)
+                                            if (not previous_word[-1] == '.') and (not previous_word[-1] == '?') and (not previous_word[-1] == '!') and (not previous_word[-3:] == '—') and (not previous_word[-3:] == '«') and (not previous_word[-3:] == '…') and (not previous_word[-1] == ':'):
+                                                Analyse.perso[sword] = [i]
+                                            else:
+                                                perso_tmp[sword] = [i]
+                                    else:
+                                        Analyse.perso.get(sword).append(i)
                         else:
-                            sword = sword.upper()
+                            sword = sword.lower()
+                            if not (sword in Analyse.not_perso):
+                                Analyse.not_perso.append(sword)
                             if(Analyse.perso.has_key(sword)):
                                 del Analyse.perso[sword]
-                                Analyse.not_perso.append(sword)
                             if perso_tmp.has_key(sword):
                                 del perso_tmp[sword]
-                                Analyse.not_perso.append(sword)
                 previous_word = word
-            # print Analyse.perso.keys()
         for index in Analyse.perso.keys():
-            occurence = Analyse.perso.get(index)
+            occurence = []
+            [occurence.append(item) for item in Analyse.perso.get(index) if item not in occurence]
             if len(occurence) == 1:
                 del Analyse.perso[index]
-                Analyse.not_perso.append(index)
-
+                if not (index in Analyse.not_perso):
+                    Analyse.not_perso.append(index)
+        print len(Analyse.not_perso)
         c = CsvMod()
         c.buildCsv(book.pages.__len__(),Analyse.perso)
