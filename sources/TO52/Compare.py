30d33a29d1f46f65eaeca2d918f5c5655338d0f5
unknown <felix.lahemade@gmail.com>
2016-01-12 21:33:48 +0100

    Updated comparaison

--- sources/TO52/Compare.py
+++ sources/TO52/Compare.py
@@ -11,7 +11,7 @@ class Compare:
         i=0
         listPerso = list()
         for row1 in reader1:
-                i=+1
+                i=i+1
                 with open('ender.csv', 'rb') as toCompare:
                     reader2 = csv.reader(toCompare, delimiter=',', quotechar='|')
                     name1 = row1[1]
@@ -22,7 +22,9 @@ class Compare:
                         tmp = re.sub("\"", '', tmp)
                         tmp = re.sub("'", '', tmp)
                         name1 = tmp
+                        j=0
                         for row2 in reader2:
+                            j=j+1
                             name2 = row2[0]
                             set1 = set(name1.split(' '))
                             set2 = set(name2.split(' '))
@@ -32,8 +34,21 @@ class Compare:
                                     item2=item2.lower()
                                     if item1 == item2:
                                         if(listPerso.__len__()>1):
-                                            if(listPerso[-1]!=name1):
-                                                listPerso.append(name1)
+                                            toAppend=1
+                                            for element in listPerso:
+                                                if(element!=name2):
+                                                    toAppend=0
+                                                else:
+                                                    toAppend=1
+                                            if(toAppend<1):
+                                                listPerso.append(name2)
                                         else:
-                                            listPerso.append(name1)
-    print(listPerso)
+                                            listPerso.append(name2)
+    print(i)
+    print(j)
+    print("On détecte "+listPerso.__len__().__str__()+"/"+j.__str__()+" des personnages présents dans l'index manuel.")
+    print("On détecte "+ (i-listPerso.__len__()).__str__() +" non pertinentes.")
+
+
+
+
